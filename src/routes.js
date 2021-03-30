4a4b9cbd8760ef403ef042eda0913532af810b88
Igor-Felipy <igor.felipy123@gmail.com>
2021-03-29 23:16:35 -0300

    criando as rotas das telas

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path='/' component={() => <h1>Hello world</h1>} />
+            <Route exact path='/login' component={() => <h1>Hello world</h1>} />
+            <Route exact path='/register' component={() => <h1>Hello world</h1>} />
+            <PrivateRoute path='/' component={() => <h1>Você esta logado</h1>} />
             <PrivateRoute path='/app' component={() => <h1>Você esta logado</h1>} />
+            <PrivateRoute path='/new_post' component={() => <h1>Você esta logado</h1>} />
+            <PrivateRoute path='/post_details' component={() => <h1>Você esta logado</h1>} />
+            <PrivateRoute path='/profile' component={() => <h1>Você esta logado</h1>} />
         </Switch>
     </BrowserRouter>
 )
