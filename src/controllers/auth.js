039a176ba6a22577b01660d52ff66cfb032a4362
Igor-Felipy <igor.felipy123@gmail.com>
2021-04-06 13:08:45 -0300

    criando o metódo de login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,1 +1,24 @@
+const { default: axios } = require('axios');
+
+const base = axios.create({
+    baseURL: 'http://localhost:3000'
+})
+
+function register(){
+    base.post('/register',{
+        'teste':'teste'
+    })
+}
+function login(){
+    base.post('/login',{
+        'username':'teste',
+        'password':'teste'
+    }).then(function (response){
+        //o response retorna ou uma confirmação de login ou uma falha de autenticação, então é necessario fazer um if para separar cada response e dai salvar o token
+        sessionStorage.setItem('@just-feelings/token',response.token)
+    })
+}
+
+
+
 export const isAuthenticated = () => true;
