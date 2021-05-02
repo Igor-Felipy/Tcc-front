1bd3af39debdca39b9af8ff6c86af876b2f78f9c
Igor-Felipy <igor.felipy123@gmail.com>
2021-05-02 18:22:25 -0300

    criando a tela de feed

--- just-feelings/src/pages/Feed.js
+++ just-feelings/src/pages/Feed.js
@@ -15,10 +15,10 @@ export default function Feed() {
             <div className="superiorBar" style={styles.superiorBar}>
                 <div style={styles.superiorButton}>
                         <button onClick={redirect} style={styles.returnButton}>
-                            <img src={Arrow} alt="Return Button" width="12" height="24"/>
+                            <img src={Arrow} alt="Returnmy Button" width="12" height="24"/>
                         </button>
                 </div>
-                    <div style={styles.superiorP}><p style={styles.p}>Registrar</p></div>
+                    <div style={styles.superiorP}><p style={styles.p}>Explorar</p></div>
                     <div>
                         <button onClick={redirect} style={styles.searchButton}>
                                 <img src={Search}  alt="search button" width="18" height="18"/>
@@ -28,25 +28,69 @@ export default function Feed() {
             <div className="content" style={styles.content}>
                 <div className="post" style={styles.post}>
                     <div className="post-superior" style={styles.postSuperior}>
-                        <img src="https://source.unsplash.com/random/64x64" alt=""/>
+                        <img src="https://source.unsplash.com/random/64x64" alt="" style={styles.postProfilePic}/>
                         <div className="post-superior-data" style={styles.postSuperiorData}>
-                            <h2>Username</h2>
-                            <p>post data</p>
+                            <h2 style={styles.postProfileName}>Username</h2>
+                            <p style={styles.postProfileData}>post data</p>
                         </div>    
-                        <div className="post-image" style={styles.postImage}>
-                            <img src="https://source.unsplash.com/random/800x600" alt=""/>
-                        </div>
-                        <div className="post-bottom">
-                            <div className="post-bottom-left" style={styles.postBottomLeft}>
-                                <button><img src={Heart} alt="" style={styles.postBottomHeart}/></button>
-                                <button><img src={Comments} alt=""/></button>
-                            </div>
-                            <div className="post-bottom-right" style={styles.postBottomRight}>
-                                <button><img src={Share} alt=""/></button>
-                            </div>
-                        </div>
                     </div>    
-                    <hr/>
+                        <div className="post-image" style={styles.postImageDiv}>
+                            <img src="https://source.unsplash.com/random/800x600" alt="" style={styles.postImage}/>
+                        </div>
+                    <div className="post-bottom" style={styles.postBottoms}>
+                        <div className="post-bottom-left" style={styles.postBottomLeft}>
+                            <button style={styles.postBottomIcons}><img src={Heart} alt="" /></button>
+                            <button style={styles.postBottomIcons}><img src={Comments} alt="" /></button>
+                        </div>
+                        <div className="post-bottom-right" style={styles.postBottomRight}>
+                            <button style={styles.postBottomIcons}><img src={Share} alt=""/></button>
+                        </div>
+                    </div>
+                    <div style={styles.riscoFinal}></div>
+                </div>  
+                <div className="post" style={styles.post}>
+                    <div className="post-superior" style={styles.postSuperior}>
+                        <img src="https://source.unsplash.com/random/64x64" alt="" style={styles.postProfilePic}/>
+                        <div className="post-superior-data" style={styles.postSuperiorData}>
+                            <h2 style={styles.postProfileName}>Username</h2>
+                            <p style={styles.postProfileData}>post data</p>
+                        </div>    
+                    </div>    
+                        <div className="post-image" style={styles.postImageDiv}>
+                            <img src="https://source.unsplash.com/random/800x600" alt="" style={styles.postImage}/>
+                        </div>
+                    <div className="post-bottom" style={styles.postBottoms}>
+                        <div className="post-bottom-left" style={styles.postBottomLeft}>
+                            <button style={styles.postBottomIcons}><img src={Heart} alt="" /></button>
+                            <button style={styles.postBottomIcons}><img src={Comments} alt="" /></button>
+                        </div>
+                        <div className="post-bottom-right" style={styles.postBottomRight}>
+                            <button style={styles.postBottomIcons}><img src={Share} alt=""/></button>
+                        </div>
+                    </div>
+                    <div style={styles.riscoFinal}></div>
+                </div>  
+                <div className="post" style={styles.post}>
+                    <div className="post-superior" style={styles.postSuperior}>
+                        <img src="https://source.unsplash.com/random/64x64" alt="" style={styles.postProfilePic}/>
+                        <div className="post-superior-data" style={styles.postSuperiorData}>
+                            <h2 style={styles.postProfileName}>Username</h2>
+                            <p style={styles.postProfileData}>post data</p>
+                        </div>    
+                    </div>    
+                        <div className="post-image" style={styles.postImageDiv}>
+                            <img src="https://source.unsplash.com/random/800x600" alt="" style={styles.postImage}/>
+                        </div>
+                    <div className="post-bottom" style={styles.postBottoms}>
+                        <div className="post-bottom-left" style={styles.postBottomLeft}>
+                            <button style={styles.postBottomIcons}><img src={Heart} alt="" /></button>
+                            <button style={styles.postBottomIcons}><img src={Comments} alt="" /></button>
+                        </div>
+                        <div className="post-bottom-right" style={styles.postBottomRight}>
+                            <button style={styles.postBottomIcons}><img src={Share} alt=""/></button>
+                        </div>
+                    </div>
+                    <div style={styles.riscoFinal}></div>
                 </div>    
             </div>      
         </div>
@@ -80,6 +124,7 @@ const styles = {
     },
     returnButton:{
         cursor:"pointer",
+        backgroundColor:"#00BFA5",
     },
     superiorP:{
         display:'flex',
@@ -92,6 +137,51 @@ const styles = {
     },
     searchButton:{
         margin:"10px",
-        backgroudColor:"#00BFA5"
+        backgroundColor:"#00BFA5"
+    },
+    post:{
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"space-between",
+        marginBottom:"30px"
+    },
+    postSuperior:{
+        display:"flex",
+        flexDirection:"row",
+        marginTop:"10px"
+    },
+    postProfilePic:{
+        width:"37px",
+        height:"37px",
+        borderRadius:"100%",
+        marginRight:"10px"
+    },
+    postProfileName:{
+        fontWeight:"bold",
+        fontSize:"13px",
+        color:"#151522"
+    },
+    postProfileData:{
+        fontWeight: "500",
+        fontSize:"12px",
+        color:"#999999"
+    },
+    postImageDiv:{
+        marginTop:"10px"
+    },
+    postImage:{
+        width:"300px",
+        height:"auto",
+        borderRadius:"10px"
+    },
+    postBottoms:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"space-between",
+
+    },
+    postBottomIcons:{
+        margin:"10px",
+        backgroundColor:"#FFF"
     },
 }
